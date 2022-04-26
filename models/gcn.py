0160887a88c20d9a4f838f64405080b26846e466
javirk <jgamtejero@gmail.com>
2022-04-26 11:53:54 +0200

    Without self loops.

--- models/gcn.py
+++ models/gcn.py
@@ -8,9 +8,9 @@ class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, output_dim):
         super(GCN, self).__init__()
         # torch.manual_seed(12345)
-        self.conv1 = GCNConv(num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, output_dim)
+        self.conv1 = GCNConv(num_features, hidden_channels, add_self_loops=False)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels, add_self_loops=False)
+        self.conv3 = GCNConv(hidden_channels, output_dim, add_self_loops=False)
         # self.lin = Linear(hidden_channels, dataset.num_classes)
 
     def forward(self, x, edge_index, batch=None):
