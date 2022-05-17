4d7bfbd626d53b12c9fe12815b566b2e23ce7a6b
javirk <jgamtejero@gmail.com>
2022-05-17 10:15:59 +0200

    Smaller GNN with embedding layer

--- models/gcn.py
+++ models/gcn.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Conv2d
+from torch.nn import Conv2d, Linear
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from einops import rearrange
@@ -9,19 +9,21 @@ class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, output_dim):
         super(GCN, self).__init__()
         # torch.manual_seed(12345)
-        self.conv1 = GCNConv(num_features, hidden_channels, add_self_loops=False)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels, add_self_loops=False)
+        self.embedding_layer = Linear(num_features, hidden_channels, bias=False)
+        self.conv1 = GCNConv(hidden_channels, hidden_channels, add_self_loops=False)
+        # self.conv2 = GCNConv(hidden_channels, hidden_channels, add_self_loops=False)
         self.conv3 = GCNConv(hidden_channels, output_dim, add_self_loops=False)
         # self.lin = Linear(hidden_channels, dataset.num_classes)
         self.conv_sal = Conv2d(output_dim, 1, kernel_size=1)
 
     def forward(self, x, adj_sparse, batch_size=None, f_h=None, f_w=None):
+        x = F.relu(self.embedding_layer(x))
         edge_index, edge_weight = adj_sparse.indices(), adj_sparse.values()
 
         x = self.conv1(x, edge_index, edge_weight)
         x = x.relu()
-        x = self.conv2(x, edge_index, edge_weight)
-        x = x.relu()
+        # x = self.conv2(x, edge_index, edge_weight)
+        # x = x.relu()
         features = self.conv3(x, edge_index, edge_weight)
 
         if batch_size is not None:
